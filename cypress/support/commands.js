403f5b7733e80787153522aef4a7b8f71f901907
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-01-18 13:42:31 +0100

    Add startCleanApplication() command for faster startups

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,11 @@ Cypress.Commands.add("startApplication", (options) => {
   cy.waitForTransition();
 });
 
+Cypress.Commands.add("startCleanApplication", (options) => {
+  localStorage.setItem('dotSrc', 'digraph {}');
+  cy.visit('http://localhost:3000/', options);
+});
+
 Cypress.Commands.add("textEditor", () => {
   return cy.get('#text-editor');
 });
